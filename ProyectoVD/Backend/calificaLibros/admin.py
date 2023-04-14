f661bbb9ec8bf3cb3ca7c4be2cd85865c981fa9c
Juan Carlos <o.llanos.juan19@gmail.com>
2023-04-14 12:45:54 -0600

    Se realiza la base de datos

--- ProyectoVD/Backend/calificaLibros/admin.py
+++ ProyectoVD/Backend/calificaLibros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Autor, Genero, Usuario, Libro, Calificacion
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Genero)
+admin.site.register(Usuario)
+admin.site.register(Libro)
+admin.site.register(Calificacion)
--- ProyectoVD/Backend/calificaLibros/models.py
+++ ProyectoVD/Backend/calificaLibros/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre =models.CharField(max_length=100)
+    apellido =models.CharField(max_length=150)
+    fecha_nacimiento =models.DateField()
+    def _str_(self):
+        return self.nombree + ' ' + self.apellido
+    
+class Genero(models.Model):
+    nombre = models.CharField(max_length=100)
+    def _str_(self):
+            return self.nombre 
+    
+class Usuario(models.Model):
+     correo = models.EmailField()
+     contrasena = models.CharField(max_length=500)
+     def __str__(self) :
+          return self.correo
+     
+class Libro(models.Model):
+     titulo = models.CharField(max_length=200)
+     sinopsis = models.TextField()
+     portada = models.FileField(upload_to='calificalibros/portadas/')
+     fecha_publicacion = models.DateField('Fecha de publicación')
+     num_pag = models.IntegerField('número de páginas', default=0)
+     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+     autores = models.ManyToManyField(Autor)
+     calificacion = models.ManyToManyField(Usuario, through='Calificacion')
+     def __str__(self):
+          return self.titulo
+     
+class Calificacion(models.Model):
+     usuarios = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+     libro = models.ForeignKey(Libro, on_delete=models.CASCADE, related_name='calificaciones')
+     calificacion = models.IntegerField()
+     comentario = models.TextField()
+     def __str__(self):
+          return str(self.calificacion) + '' + self.comentario
