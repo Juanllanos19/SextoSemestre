652d07facf9f145ee15032ae76f869e87afd2ca7
Juan Carlos <o.llanos.juan19@gmail.com>
2023-04-16 19:15:32 -0600

    ya se hizo la prueba 5

--- jasmine-standalone-4.6.0/src/user.js
+++ jasmine-standalone-4.6.0/src/user.js
@@ -3,9 +3,41 @@ class User {
     lastName;
     middleName;
 
-    constructor(data = {}){
+    constructor(data, userService){
         this.firstName = data.firstName || '';
         this.lasttName = data.lastName || '';
         this.middleName = data.middleName || '';
+        this.id = data.id;
+        this.userService = userService;
+    }
+
+    get fullName() {
+        if(this.middleName.leght > 0) {
+            return `${this.firsName} ${this.middleName} ${this.lastName}`;
+        }
+
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    async getMyFullUserData() {
+        return this.userService.getUserByID(this.id);
+    }
+
+    sayMyName(){
+        window.alert(this.fullName);
+    }
+
+    getCodeName(){
+        alert(this.fullName);
+    }
+
+    getCodeName() {
+        const isATestingGod = confirm('Are you a testing god?');
+
+        if(isATestingGod){
+            return 'TESTING GOOD!';
+        }else {
+            return 'Scrub skipping tests in his best friends ride!';
+        }
     }
 }
